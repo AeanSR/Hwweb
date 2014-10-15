277ca21baa1fe294b5de0892ecee904975f982be
Andrew <lichundian@gmail.com>
2014-10-15 10:57:15 +0800

    add the homework test account conceal unpublished homework

--- dataInit/testDataImport.sh
+++ dataInit/testDataImport.sh
@@ -2,5 +2,8 @@
 
 #===用于测试的数据库(test_hwweb),可能会清空========================================
 
-#导入users到test_hwweb数据库
-mongoimport -h localhost --port 27017 -d test_hwweb -c users --jsonArray --file test_users.mongo
+#导入test_game_users到test_hwweb数据库(游戏测试)
+mongoimport -h localhost --port 27017 -d test_hwweb -c users --jsonArray --file test_game_users.mongo
+
+#导入test_homework_users到hwweb数据库(作业测试),作业测试角色和普通角色一致
+mongoimport -h localhost --port 27017 -d hwweb -c users --jsonArray --file test_homework_user.mongo
