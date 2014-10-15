52e302c8d9bc76fe4e81b0a39e6d8bfc73039c60
root <root@localhost.localdomain>
2014-10-15 16:52:29 +0800

    modify the backup and restore shell script specially for the server configuration

--- maintain/backup.sh
+++ maintain/backup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 dumpPath=$(cd `dirname $0`; pwd)/backupDump-`date "+%Y-%m-%d_%H-%M-%S"`
-echo $dumpPath >> /home/andrew/ccc
-MONGO_HOME=/usr/local/mongodb-linux-i686-2.6.5
+MONGO_HOME=/usr/local/mongodb-linux-x86_64-2.6.4
 cmd="$MONGO_HOME/bin/mongodump -h localhost --port 27017 -d hwweb -o $dumpPath"
 echo $cmd && ${cmd}
--- maintain/restore.sh
+++ maintain/restore.sh
@@ -3,7 +3,7 @@ if [ $# -lt 1 ]; then
     echo "usage: ./restore.sh sourcePath"
     exit
 fi
-
 dumpPath=$1
-cmd="mongorestore -h localhost --port 27017 -d hwweb $dumpPath/hwweb"
+MONGO_HOME=/usr/local/mongodb-linux-x86_64-2.6.4
+cmd="$MONGO_HOME/bin/mongorestore -h localhost --port 27017 -d hwweb $dumpPath/hwweb"
 echo $cmd && ${cmd}
